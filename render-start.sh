81564d265a7e2e7708aaa9840a919c42749b8601
CDOM24 <164239045+CDOM24@users.noreply.github.com>
2026-05-21 21:28:27 -0500

    R

--- render-start.sh
+++ render-start.sh
@@ -1,6 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+mkdir -p /var/www/html/database
+mkdir -p /var/www/html/storage/framework/views
+mkdir -p /var/www/html/storage/framework/cache
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/logs
+mkdir -p /var/www/html/bootstrap/cache
 
 touch /var/www/html/database/database.sqlite
+
+chmod -R 775 /var/www/html/storage
+chmod -R 775 /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/database
 
 php artisan migrate --force
 php artisan storage:link || true
